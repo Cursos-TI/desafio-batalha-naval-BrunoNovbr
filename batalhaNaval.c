6291a86d79859745a340b3f79091cd756c9a8af5
BrunoNovbr <bruninhtmani@gmail.com>
2025-08-05 02:08:41 +0000

    Código foi melhorado, falta ainda ultimas mudanças

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
+
+#define LINHAS 10
+#define COLUNAS 10
+
+
+int main () {
     //Array e Matriz para exibir o tabuleiro do jogo
-    char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int tabuleiro[10][10] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    char coluna[LINHAS] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int tabuleiro[LINHAS][COLUNAS] = {
     };
 
-    //Posicionamento dos navios.
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
+    //Posionamento dos barcos
+    //Navio Horizontal posicionado na LINHA 5, nas colunas C, D e E.
+    int navioHorizontal[3] = {3, 3, 3};
+    
+
 
     //Impressão da linha que mostra a marcação das colunas do Tabuleiro
     printf ("  ");
-    for (int i = 0; i < 10; i++){
+    for (int i = 0; i < COLUNAS; i++){
         printf("%c ", coluna[i]);
     };
 
     printf("\n");
     //Impressão do restante do tabuleiro
-    for (int j = 0; j < 10; j++){
+    for (int j = 0; j < LINHAS; j++){
         printf("%d ", j + 1); // <- Impressão da coluna que mostra as marcação das linhas
-        for (int k = 0; k < 10; k++) {
+        for (int k = 0; k < COLUNAS; k++) {
             printf("%d ", tabuleiro[j][k]);
         }
         printf("\n");
